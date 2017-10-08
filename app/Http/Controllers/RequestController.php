082677780b0c1634dcec182c64f9a34db625fcc7
minhtrang <trang.btm31@gmail.com>
2017-10-08 23:42:00 +0700

    [Request] Request vehicle # - Request from needers. - Request from the one has vehicle. Bug: date.

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -35,16 +35,28 @@ class RequestController extends Controller
 				'device_id' => $request->get('device_id'),
 			]
 		);
-		if($request->get('vehicle_type') == 0){
-			$hasVehicle = RequestFromNeeders::join('Users', 'request_from_needers.users_id', '=', 'users.id')
-				->select('users.id', '', '')
-				->where('vehicle_type', '=!', 0)->get();
-			return $this->success([
-				"user_vehicle_list" => $hasVehicle
-			],
-				200);
-		}
-		else {
+		if ($request->get('vehicle_type') == 0) {
+			$hasVehicle = RequestFromNeeders::join('users', 'request_from_needers.user_id', '=', 'users.id')
+				->select(
+					'users.id',
+					'users.phone',
+					'users.email',
+					'users.name',
+					'users.address',
+					'users.gender',
+					'users.birthday',
+					'users.avatar_link',
+					'request_from_needers.vehicle_type'
+				)
+				->where('request_from_needers.vehicle_type', '!=', '0')->get();
+
+			return $this->success(
+				[
+					"user_vehicle_list" => $hasVehicle,
+				],
+				200
+			);
+		} else {
 			return $this->success('', 200);
 		}
 
