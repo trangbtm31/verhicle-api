150eccd9a9255309fcdbb124418d8357118cb549
minhtrang <trangbtm@dirox.net>
2017-11-07 14:45:45 +0700

    Update database configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -27,6 +27,7 @@ $app = new Laravel\Lumen\Application(
 $app->withFacades();
 
 $app->withEloquent();
+$app->configure('database');
 
 /*
 |--------------------------------------------------------------------------
