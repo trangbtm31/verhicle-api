33c4407e027e3d001aa65903e59e096976dafbf7
minhtrang <trangbtm@dirox.net>
2017-11-07 18:36:02 +0700

    Fix databse contructor

---
+++ database/migrations/2017_11_07_112011_update_fcm_infos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateFcmInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('fcm_infos', function (Blueprint $table) {
+            //
+            $table->string('token')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('fcm_infos', function (Blueprint $table) {
+            $table->dropColumn('token');
+            //
+        });
+    }
+}
---
+++ database/migrations/2017_11_07_112913_rename_fcm_infos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RenameFcmInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::rename('fcm_infos', 'device_infos');
+        Schema::table('device_infos', function (Blueprint $table) {
+            //
+            $table->string('device_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('device_infos', function (Blueprint $table) {
+            $table->dropColumn('device_id');
+            //
+        });
+    }
+}
