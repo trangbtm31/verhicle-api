10a73a1156c8a7573e570a47e23933ce9d139c9f
minhtrang <trangbtm@dirox.net>
2017-11-07 11:31:46 +0700

    Test

--- app/Http/Controllers/RatingController.php
+++ app/Http/Controllers/RatingController.php
@@ -9,47 +9,60 @@ use Illuminate\Http\Request;
 
 class RatingController extends Controller
 {
-	/**
-	 * Create a new controller instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		//
-	}
+    /**
+     * @var Rating
+     */
+    protected $rating;
 
-	public function doVote(Request $request)
-	{
-		$rating = new Rating();
-		$totalRatingValue = 0;
-		$rating->create(
-			[
-				'user_id' => $request->get('user_id'),
-				'journey_id' => $request->get('journey_id'),
-				'rating_value' => $request->get('rating_value'),
-			]
-		);
-		$ratingValues = $rating->where('journey_id', '=', $request->get('journey_id'))->get();
-		$totalRating = $ratingValues->count();
-		$lastRating = $rating->orderBy('id', 'desc')->first();
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+        $this->rating = new Rating();
+    }
 
-		foreach ($ratingValues as $ratingValue) {
-			$totalRatingValue += $ratingValue->rating_value;
-		}
-		$argRating = round($totalRatingValue / $totalRating, 1);
-		$result = [
-			'update_date' => $lastRating->updated_at,
-			'total_rating' => $argRating,
-		];
+    public function doVote(Request $request)
+    {
+        $rating = $this->rating;
+        $totalRatingValue = 0;
+        $rating->create(
+            [
+                'user_id' => $request->get('user_id'),
+                'journey_id' => $request->get('journey_id'),
+                'rating_value' => $request->get('rating_value'),
+                'comment' => $request->get('comment')
+            ]
+        );
+        $ratingValues = $rating->where('journey_id', '=', $request->get('journey_id'))->get();
+        $totalRating = $ratingValues->count();
+        $lastRating = $rating->orderBy('id', 'desc')->first();
 
-		return $this->success(
-			"rating_info",
-			$result,
-			200
-		);
+        foreach ($ratingValues as $ratingValue) {
+            $totalRatingValue += $ratingValue->rating_value;
+        }
+        $argRating = round($totalRatingValue / $totalRating, 1);
+        $result = [
+            'update_date' => $lastRating->updated_at,
+            'total_rating' => $argRating,
+        ];
 
-	}
+        return $this->success(
+            "rating_info",
+            $result,
+            200
+        );
 
-	//
+    }
+
+    public function getCommentList($journey_id)
+    {
+        $rating = $this->rating;
+        return $rating->where('journey_id', '=', $journey_id)->orderBy('id', 'desc')->get();
+    }
+
+    //
 }
