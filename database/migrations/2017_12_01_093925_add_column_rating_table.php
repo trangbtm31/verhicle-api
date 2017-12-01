14efbb221ba20b4150c441db30f9a8730fdd2e4d
minhtrang <trangbtm@dirox.net>
2017-12-01 18:22:54 +0700

    [Rating History] Build user's rating history

---
+++ database/migrations/2017_12_01_093925_add_column_rating_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnRatingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('rating', function (Blueprint $table) {
+            $table->integer('vehicle_type');
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('rating', function (Blueprint $table) {
+            //
+        });
+    }
+}
